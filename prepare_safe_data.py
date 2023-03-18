4c00a96dd568dd9ac8caca4842a2ea9c9dab6b88
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2023-03-18 14:54:24 -0400

    debug data preprosessing

--- prepare_safe_data.py
+++ prepare_safe_data.py
@@ -9,6 +9,7 @@ import numpy as np
 import json
 import os
 import sys
+from tqdm import tqdm
 
 
 article_folder = "/afs/crc.nd.edu/group/cvrl/scratch_49/jhuang24/" \
@@ -48,9 +49,9 @@ def process_one_embedding(embedding,
     elif embedding.shape[0] < required_size:
         # Case 2: Caption is shorter than required length - fill up to n with zeroes
         filler = np.zeros((required_size - embedding.shape[0], embedding_length))
-        embedding = np.stack((embedding, filler))
+        embedding = np.concatenate((embedding, filler))
 
-    assert (embedding.shape[0] == required_size) and (img_embedding.shape[1] == embedding_length)
+    assert (embedding.shape[0] == required_size) and (embedding.shape[1] == embedding_length)
 
     return embedding
 
@@ -93,7 +94,8 @@ def generate_training_npy(article_folder,
         all_samples = os.listdir(dir_full_path)
 
         # TODO: Process each sample
-        for one_sample in all_samples:
+        for i in tqdm(range(len(all_samples))):
+            one_sample = all_samples[i]
             # 0. Get full path for all files
             one_sample_dir = os.path.join(dir_full_path, one_sample)
 
@@ -113,42 +115,49 @@ def generate_training_npy(article_folder,
 
                 img_embedding = process_one_embedding(embedding=img_embedding,
                                                       required_size=image_size)
+                img_embedding = np.reshape(img_embedding,
+                                           (1, img_embedding.shape[0], img_embedding.shape[1]))
 
-                if all_img == None:
+                if all_img is None:
                     all_img = img_embedding
                 else:
-                    all_img = np.stack((all_img, img_embedding))
+                    all_img = np.concatenate((all_img, img_embedding))
+
 
                 # TODO: process headline embeddings to the desired length
                 headline_embedding = np.load(os.path.join(one_sample_dir, "headline.npy"))
                 headline_embedding = process_one_embedding(embedding=headline_embedding,
                                                            required_size=head_size)
+                headline_embedding = np.reshape(headline_embedding,
+                                                (1, headline_embedding.shape[0], headline_embedding.shape[1]))
 
-                if all_headline == None:
+                if all_headline is None:
                     all_headline = headline_embedding
                 else:
-                    all_headline = np.stack((all_headline, headline_embedding))
+                    all_headline = np.concatenate((all_headline, headline_embedding))
 
                 # TODO: process body embeddings to the desired length
                 body_embedding = np.load(os.path.join(one_sample_dir, "body.npy"))
                 body_embedding = process_one_embedding(embedding=body_embedding,
                                                        required_size=body_size)
+                body_embedding = np.reshape(body_embedding,
+                                            (1, body_embedding.shape[0], body_embedding.shape[1]))
 
-                if all_body == None:
+                if all_body is None:
                     all_body = body_embedding
                 else:
-                    all_body = np.stack((all_body, body_embedding))
+                    all_body = np.concatenate((all_body, body_embedding))
 
                 # TODO: process data label
                 if label_name == "real":
-                    one_label = np.array([1, 0])
+                    one_label = np.array([[1, 0]])
                 else:
-                    one_label = np.array([0, 1])
+                    one_label = np.array([[0, 1]])
 
-                if all_labels == None:
+                if all_labels is None:
                     all_labels = one_label
                 else:
-                    all_labels = np.stack((all_labels, one_label))
+                    all_labels = np.concatenate((all_labels, one_label))
 
             # Go to the next sample if there is no img embedding
             else:
